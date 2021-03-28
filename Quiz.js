26afff3a968fd162d765067acf0aa4654e254968
luctr <dinh.luc.3192@gmail.com>
2021-03-28 10:16:59 +0700

    add

--- Quiz.js
+++ Quiz.js
@@ -7,28 +7,28 @@ class Quiz{
 }
 let quizs=[];
 let arr =new Quiz("image/bactinh.png","bạc tình","Trong hình có bạc và trái tim")
-let arr1= new Quiz("image/bahoa.png","ba hoa","Chỉ có hoa")
+let arr1= new Quiz("image/trangluoiliem.png","trăng lưỡi liềm","Hình trăng")
 let arr2= new Quiz("image/baocao.png","báo cáo","Con cáo")
 let arr3= new Quiz("image/baola.png","bao la","cái bao đang làm gì")
 let arr4= new Quiz("image/cadao.png","ca dao","Trong hình có gì thì câu trả lời ở đó")
-let arr5= new Quiz("image/canbang.png","cân bằng","Trong hình có gì nào")
+let arr5= new Quiz("image/obama.jpg","ô ba ma","Tên một vị tổng thống" )
 let arr6= new Quiz("image/chidiem.png","chỉ điểm","Trong hình có gì thì câu trả lời ở đó")
-let arr7= new Quiz("image/cungcau.png","cung cầu","Một câu thể hiện kinh doanh")
+let arr7= new Quiz("image/mynhanngu.jpg","mỹ nhân ngư","Cờ nước mỹ và mỹ nhân")
 let arr81= new Quiz("image/loinguocdong.png","lội ngược dòng","Cái chân đang làm gì")
 let arr8= new Quiz("image/matma.png","mật mã","Con ngựa và lọ mật")
-let arr9= new Quiz("image/mynhanngu.jpg","mỹ nhân ngư","Cờ nước mỹ và mỹ nhân")
-let arr10= new Quiz("image/obama.jpg","obama","Tên một vị tổng thống")
-let arr11= new Quiz("image/trangluoiliem.png","trăng lưỡi liềm","Hình trăng")
+let arr9= new Quiz("image/cungcau.png","cung cầu","Một câu thể hiện kinh doanh")
+let arr10= new Quiz("image/canbang.png","cân bằng","Trong hình có gì nào")
+let arr11= new Quiz("image/bahoa.png","ba hoa","Chỉ có hoa")
 quizs.push(arr);
 quizs.push(arr1);
-quizs.push(arr2);
-quizs.push(arr3);
-quizs.push(arr4);
-quizs.push(arr5);
-quizs.push(arr6);
-quizs.push(arr7);
-quizs.push(arr81);
-quizs.push(arr8);
-quizs.push(arr9);
-quizs.push(arr10);
-quizs.push(arr11);
+// quizs.push(arr2);
+// quizs.push(arr3);
+// quizs.push(arr4);
+// quizs.push(arr5);
+// quizs.push(arr6);
+// quizs.push(arr7);
+// quizs.push(arr81);
+// quizs.push(arr8);
+// quizs.push(arr9);
+// quizs.push(arr10);
+// quizs.push(arr11);
--- game.js
+++ game.js
@@ -1,50 +1,92 @@
-let count=Math.floor(Math.random() * ((quizs.length-1) - 0 + 1) + 0);
-let  count1=0;
-let point=0;
-
+let count = Math.floor(Math.random() * ((quizs.length - 1) - 0 + 1) + 0);
+let count1 = 0;
+let point = 0;
+let abc = [];
 let getQuizs = document.getElementById("img");
 getQuizs.setAttribute("src", quizs[count].img);
 
 function point1() {
-    document.getElementById("sp1").innerHTML ="Điểm của bạn " + point;
+    document.getElementById("sp1").innerText = "Điểm của bạn " + point;
 }
-function Support(){
-    document.getElementById("text").innerText=quizs[count].note;
+
+function Support() {
+    document.getElementById("text").innerText = quizs[count].note;
 }
+
 function Reset() {
-   let a = confirm("Bạn có chắc chắn muốn reset không")
-    if (a){
+    let a = confirm("Bạn có chắc chắn muốn reset không")
+    if (a) {
         location.reload();
     }
 
 }
-function next(){
-    if (count1==0){
-        count++;
+
+function next() {
+
+    count = Math.floor(Math.random() * ((quizs.length - 1) - 0 + 1) + 0);
+    while (abc.includes(count)) {
+        count = Math.floor(Math.random() * ((quizs.length - 1) - 0 + 1) + 0);
+    }
+
+    if (count1 == 0) {
+        // count++;
         count1++;
+        getQuizs.setAttribute("src", quizs[count].img);
+    } else {
+        alert("hết lượt");
+    }
+
+}
+
+function next2() {
+
+    count = Math.floor(Math.random() * ((quizs.length - 1) - 0 + 1) + 0);
+    while (abc.includes(count)) {
+        if(abc.length === quizs.length)
+            break
+
+
+        count = Math.floor(Math.random() * ((quizs.length - 1) - 0 + 1) + 0);
+    }
+    if (count < quizs.length) {
+        console.log(quizs[count])
         getQuizs.setAttribute("src", quizs[count].img);
     }
 
-    else{
-        alert("hết lượt" );
-    }
-
-}function next2(){
-    count++;
-    getQuizs.setAttribute("src",quizs[count].img);
-    if (count == quizs.length-1 ){
+    if (abc.length == quizs.length-1) {
         alert("câu hỏi cuối cùng rồi nè")
     }
+
+    if (abc.length == quizs.length){
+        document.getElementById("kq").style.display='none'
+        document.getElementById("ip-1").style.display='none'
+        document.getElementById("win").style.display='block'
+
+    }
 }
+
+function handleClose(){
+    document.getElementById("win").style.display='none'
+    count = Math.floor(Math.random() * ((quizs.length - 1) - 0 + 1) + 0);
+    count1 = 0;
+    point = 0;
+    abc = [];
+
+}
+
 function Result() {
-    let check=document.getElementById("ip-1").value == quizs[count].correct
-            if(check){
-                point++;
-                alert("đúng rồi")
-                next2();
-                point1();
-            }else{
-                alert("sai rồi")
-            }
+    let value = document.getElementById("ip-1").value
+    let check = value === quizs[count].correct
+    if (check) {
+        abc.push(count)
+        point++;
+        alert("đúng rồi")
+        next2();
+        point1();
+        document.getElementById("ip-1").value = "";
+
+    } else {
+        alert("sai rồi")
+    }
 }
 
